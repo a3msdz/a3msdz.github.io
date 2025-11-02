a278cf8f77303eed2bc13948896d52dea05749d2
a3msdz <151013727+a3msdz@users.noreply.github.com>
2025-11-02 13:26:14 +0700

    Update config.js

--- my_love/config.js
+++ my_love/config.js
@@ -1,17 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất vốn lạ thường\nMà sao em cứ đi nhầm đường
+    \nLạc vào tim anh lẻ loi
+    \nĐằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnYes: 'Vẫn cứ là thích anh 🥰',
+    btnNo: 'Không, Anh trai à 😒',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh nhỉ ?',
+    btnReply: 'Gửi cho anh 😘',
+    reply: 'Yêu thì yêu mà không yêu thì yêu hehe',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
